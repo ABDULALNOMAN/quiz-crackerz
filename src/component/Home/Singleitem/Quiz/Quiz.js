15afbee4650e5eb7ac6c3854724d0c6296653ab2
noman <nomanmd1st2001@gmail.com>
2022-10-11 18:10:55 +0600

    added toast

--- src/component/Home/Singleitem/Quiz/Quiz.js
+++ src/component/Home/Singleitem/Quiz/Quiz.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import Quizvalue from './Quizvalue/Quizvalue';
 import { EyeIcon } from '@heroicons/react/24/solid'
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+
+export const CallContext = createContext()
 const Quiz = ({ data }) => {
-    const { question, options } = data
+    const { correctAnswer, question, options } = data
+    console.log(data)
+    const handleCheck = (option) => {
+        const item = correctAnswer === option;
+        console.log(item)
+        if (item) {
+            toast('true')
+        }
+        else {
+            toast('false')
+        }
+    } 
+    const handleTrue = () => {
+        toast(correctAnswer)
+    }
     return (
-        <div className='text-center mt-10 p-6 rounded-3xl shadow-md shadow-cyan-600 relative' data-theme={'night'}>
-            <h1 className='text-2xl mt-4'>{question}</h1>
-            <div className='absolute top-2 right-2'>
-                <EyeIcon className='h-6 w-6'></EyeIcon>
+        <CallContext.Provider value={handleCheck}>
+            <div className='text-center mt-10 p-6 rounded-3xl shadow-md shadow-cyan-600 relative' data-theme={'night'}>
+                <h1 className='text-2xl mt-4'>{question}</h1>
+                <div className='absolute top-2 right-2'>
+                    <EyeIcon onClick={handleTrue} className='h-6 w-6'></EyeIcon>
+                    <ToastContainer></ToastContainer>
+                </div>
+                <div className='grid grid-cols-2 gap-3 mt-10'>
+                    {
+                        options.map(option => <Quizvalue option={option}></Quizvalue>)
+                    }
+                </div>
             </div>
-            <div className='grid grid-cols-2 gap-3 mt-10'>
-                {
-                    options.map(option => <Quizvalue option={option}></Quizvalue>)
-                }
-            </div>
-        </div>
+        </CallContext.Provider>
     );
 };
 
--- src/component/Home/Singleitem/Quiz/Quizvalue/Quizvalue.js
+++ src/component/Home/Singleitem/Quiz/Quizvalue/Quizvalue.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ToastContainer } from 'react-toastify';
+import { CallContext } from '../Quiz';
 
 const Quizvalue = ({ option }) => {
+    const handleCheck = useContext(CallContext)
     return (
-        <div className='border p-3 text-lg rounded-md hover:bg-cyan-700 hover:text-white'>
-            <p>{option}</p>
+        <div>
+            <button onClick={() => handleCheck(option)} className='w-full h-full border p-3 text-lg rounded-md hover:bg-cyan-700 hover:text-white'>{option}</button>
+            <ToastContainer />
         </div>
     );
 };
