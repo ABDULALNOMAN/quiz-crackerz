30750316571e3ee6fd337b082e272b74b6b5bd52
noman <nomanmd1st2001@gmail.com>
2022-10-11 22:40:13 +0600

    responsive nevbar added

--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h1>hello bog</h1>
-        </div>
+        <dl>
+            <dt>what is the purpess of react router</dt> 
+            <dd>React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL</dd>
+            <dt>how </dt> 
+            <dd></dd>
+            <dt></dt> 
+            <dd></dd>
+        </dl>
     );
 };
 
--- src/component/Layout/Navbar/Navbar.js
+++ src/component/Layout/Navbar/Navbar.js
@@ -1,15 +1,28 @@
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div className='w-full bg-cyan-700'>
-            <div className="navbar container mx-auto text-white" >
-                <div className="flex-1">
-                    <a className="btn btn-ghost normal-case text-3xl font-bold italic ">queezy</a>
+        <div className='bg-cyan-700 text-white'>
+            <div className="navbar flex justify-between container mx-auto">
+                <div className="navbar-start">
+                    <a className="btn btn-ghost normal-case text-3xl font-bold italic">queezy</a>
                 </div>
-                <div className="flex-none">
-                    <ul className="menu menu-horizontal p-0">
+                <div className="navbar-end hidden md:flex">
+                    <div>
+                        <ul className="menu menu-horizontal p-0">
+                            <li><Link to={'/'}>Topics</Link></li>
+                            <li><Link to={"/status"}>Statistics</Link></li>
+                            <li><Link to={'/blog'}>Blog</Link></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="dropdown mr-6">
+                     <label tabIndex={0} className="btn btn-ghost md:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-cyan-700 rounded-box w-52">
                         <li><Link to={'/'}>Topics</Link></li>
                         <li><Link to={"/status"}>Statistics</Link></li>
                         <li><Link to={'/blog'}>Blog</Link></li>
